e67d76186252ce199306f90abac896583e321cd8
Ankush <deshwalankush23@gmail.com>
2025-04-23 16:51:09 +0530

    Cors configrued

--- authentication-service/src/index.ts
+++ authentication-service/src/index.ts
@@ -1,6 +1,14 @@
 import "dotenv/config";
 import app from "./app";
 import { PORT } from "./env";
+import cors from "cors";
+
+app.use(cors({
+  origin: 'http://localhost:3000',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+}))
 
 app.listen(PORT, () => {
   console.log(`Auth service running on port ${PORT}`);
